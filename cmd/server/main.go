76da0036a79768ff2c8f8a965f63b4d993de1cff
Philip Kazakov <fliptv97@gmail.com>
2025-05-30 21:26:12 +0300

    feat: simple http server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is required")
+	}
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	fmt.Printf("Starting server on :%s", port)
+	err = http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/fliptv97/notepad-server
 
 go 1.24.3
+
+require github.com/joho/godotenv v1.5.1 // indirect
