f6b53bda7d5edd738d302320056d24b04d27208b
Philip Kazakov <fliptv97@gmail.com>
2025-06-02 02:12:53 +0300

    refactor: streamline error handling in NoteHandler methods

--- internal/handlers/note-handler.go
+++ internal/handlers/note-handler.go
@@ -27,27 +27,26 @@ func (nh *NoteHandler) Create(w http.ResponseWriter, r *http.Request) {
 		Title   string `json:"title"`
 		Content string `json:"content"`
 	}
+
 	var reqBody RequestBody
+
 	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		nh.respondWithError(w, http.StatusBadRequest)
 		return
 	}
 
 	if reqBody.Title == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("'title' is required field"))
-		return
-	}
-	if reqBody.Content == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("'content' is required field"))
+		nh.respondWithError(w, http.StatusBadRequest, "'title' is required field")
 		return
 	}
 
-	_, err := nh.repo.Create(r.Context(), reqBody.Title, reqBody.Content)
-	if err != nil {
-		fmt.Printf("[ERROR] POST /note: %s\n", err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
+	if reqBody.Content == "" {
+		nh.respondWithError(w, http.StatusBadRequest, "'content' is required field")
+		return
+	}
+
+	if _, err := nh.repo.Create(r.Context(), reqBody.Title, reqBody.Content); err != nil {
+		nh.respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("POST /note: %s\n", err.Error()))
 		return
 	}
 
@@ -56,17 +55,16 @@ func (nh *NoteHandler) Create(w http.ResponseWriter, r *http.Request) {
 
 func (nh *NoteHandler) GetAll(w http.ResponseWriter, r *http.Request) {
 	notes, err := nh.repo.GetAll(r.Context())
+
 	if err != nil {
-		fmt.Printf("[ERROR] GET /note: %s\n", err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
+		nh.respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("GET /note: %s\n", err.Error()))
 		return
 	}
 
 	w.Header().Add("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(notes)
-	if err != nil {
-		fmt.Printf("[ERROR] GET /note: %s\n", err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
+
+	if err = json.NewEncoder(w).Encode(notes); err != nil {
+		nh.respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("GET /note: %s\n", err.Error()))
 		return
 	}
 }
@@ -74,28 +72,28 @@ func (nh *NoteHandler) GetAll(w http.ResponseWriter, r *http.Request) {
 func (nh *NoteHandler) GetById(w http.ResponseWriter, r *http.Request) {
 	rawId := r.PathValue("id")
 	id, err := uuid.Parse(rawId)
+
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Provided id is invalid"))
+		nh.respondWithError(w, http.StatusBadRequest, "Provided id is invalid")
 		return
 	}
 
 	note, err := nh.repo.GetById(r.Context(), id)
+
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
-			w.WriteHeader(http.StatusNotFound)
+			nh.respondWithError(w, http.StatusNotFound)
 			return
 		}
 
-		fmt.Printf("[ERROR] GET /note/%s: %s\n", rawId, err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
+		nh.respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("GET /note/%s: %s\n", rawId, err.Error()))
 		return
 	}
 
 	w.Header().Add("Content-Type", "application/json")
+
 	if err := json.NewEncoder(w).Encode(note); err != nil {
-		fmt.Printf("[ERROR] GET /note/%s: %s\n", rawId, err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
+		nh.respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("GET /note/%s: %s\n", rawId, err.Error()))
 		return
 	}
 }
@@ -103,9 +101,9 @@ func (nh *NoteHandler) GetById(w http.ResponseWriter, r *http.Request) {
 func (nh *NoteHandler) Update(w http.ResponseWriter, r *http.Request) {
 	rawId := r.PathValue("id")
 	id, err := uuid.Parse(rawId)
+
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Provided id is invalid"))
+		nh.respondWithError(w, http.StatusBadRequest, "Provided id is invalid")
 		return
 	}
 
@@ -113,46 +111,53 @@ func (nh *NoteHandler) Update(w http.ResponseWriter, r *http.Request) {
 		Title   *string `json:"title,omitempty"`
 		Content *string `json:"content,omitempty"`
 	}
+
 	var reqBody requestBody
+
 	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
-		fmt.Printf("[ERROR] PATCH /note/%s: %s\n", rawId, err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
+		nh.respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("PATCH /note/%s: %s\n", rawId, err.Error()))
 		return
 	}
+
 	if reqBody.Title == nil && reqBody.Content == nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Request should contain at least one property to update"))
+		nh.respondWithError(w, http.StatusBadRequest, "Request should contain at least one property to update")
 		return
 	}
 
 	note, err := nh.repo.GetById(r.Context(), id)
+
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
-			w.WriteHeader(http.StatusNotFound)
+			nh.respondWithError(w, http.StatusNotFound)
 			return
 		}
-		fmt.Printf("[ERROR] PATCH /note/%s: %s\n", rawId, err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
+
+		nh.respondWithError(
+			w,
+			http.StatusInternalServerError,
+			fmt.Sprintf("PATCH /note/%s: %s\n", rawId, err.Error()),
+		)
 		return
 	}
 
 	if reqBody.Title != nil {
 		note.Title = *reqBody.Title
 	}
+
 	if reqBody.Content != nil {
 		note.Content = *reqBody.Content
 	}
+
 	note.UpdatedAt = time.Now().UTC()
 	err = nh.repo.Update(r.Context(), id, note.Title, note.Content)
+
 	if err != nil {
-		fmt.Printf("[ERROR] PATCH /note/%s: %s\n", rawId, err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
+		nh.respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("PATCH /note/%s: %s\n", rawId, err.Error()))
 		return
 	}
 
 	if err := json.NewEncoder(w).Encode(note); err != nil {
-		fmt.Printf("[ERROR] PATCH /note/%s: %s\n", rawId, err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
+		nh.respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("PATCH /note/%s: %s\n", rawId, err.Error()))
 		return
 	}
 }
@@ -160,27 +165,40 @@ func (nh *NoteHandler) Update(w http.ResponseWriter, r *http.Request) {
 func (nh *NoteHandler) Delete(w http.ResponseWriter, r *http.Request) {
 	rawId := r.PathValue("id")
 	id, err := uuid.Parse(rawId)
+
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Provided id is invalid"))
+		nh.respondWithError(w, http.StatusBadRequest, "Provided id is invalid")
 		return
 	}
 
 	if _, err = nh.repo.GetById(r.Context(), id); err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
-			w.WriteHeader(http.StatusNotFound)
+			nh.respondWithError(w, http.StatusNotFound)
 			return
 		}
-		fmt.Printf("[ERROR] DELETE /note/%s: %s\n", rawId, err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
+
+		nh.respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("DELETE /note/%s: %s\n", rawId, err.Error()))
 		return
 	}
 
 	if err = nh.repo.Delete(r.Context(), id); err != nil {
-		fmt.Printf("[ERROR] DELETE /note/%s: %s\n", rawId, err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
+		nh.respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("DELETE /note/%s: %s\n", rawId, err.Error()))
 		return
 	}
 
 	w.WriteHeader(http.StatusNoContent)
+}
+
+func (nh *NoteHandler) respondWithError(w http.ResponseWriter, status int, messages ...string) {
+	w.WriteHeader(status)
+
+	if len(messages) > 0 {
+		return
+	}
+
+	if status >= http.StatusInternalServerError {
+		fmt.Printf("[ERROR] %s\n", messages[0])
+	} else if messages[0] != "" {
+		w.Write([]byte(messages[0]))
+	}
 }
